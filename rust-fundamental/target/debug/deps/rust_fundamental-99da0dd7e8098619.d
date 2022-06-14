135c811c43977b2a487d9e5cf12fc95e26c7c5db
andiahmads <andi.fivesco@gmail.com>
2022-06-14 21:19:23 +0700

    initial commit

---
+++ rust-fundamental/target/debug/deps/rust_fundamental-99da0dd7e8098619.d
@@ -0,0 +1,5 @@
+/home/domv/Code/rust/rust-explore/rust-fundamental/target/debug/deps/rust_fundamental-99da0dd7e8098619: src/main.rs
+
+/home/domv/Code/rust/rust-explore/rust-fundamental/target/debug/deps/rust_fundamental-99da0dd7e8098619.d: src/main.rs
+
+src/main.rs:
