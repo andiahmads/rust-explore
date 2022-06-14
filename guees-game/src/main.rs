4438a02e52693b1fcffdb3f31d0bac252624e4c3
andiahmads <andi.fivesco@gmail.com>
2022-06-14 22:22:11 +0700

    complate tutorial

--- guees-game/src/main.rs
+++ guees-game/src/main.rs
@@ -1,4 +1,46 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io; // method perbadingan bertipe enum
+
 fn main() {
-    println!("Hello, world!");
-    println!("Hello, world!");
+    println!("Guess the number!");
+    let secret_number = rand::thread_rng().gen_range(1..100);
+    println!("The secret_number is: {}", secret_number);
+
+    
+    loop {
+        println!("Please input your guess");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("failed to read line");
+
+        println!("your guess: {}", guess);
+
+     
+
+        // lakukan teknik shadowing yg berfungsi untuk convert string ke INT
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num, // value cocok
+            Err(_) => continue, //tidak melakukan apapun
+        };
+
+        println!("your guessed {}", guess);
+
+
+        /*
+            match dapat digunakan untuk menjalankan kode secara kondisional.
+            dan me return nilai enum
+        */
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("to small!"),
+            Ordering::Greater => println!("to Big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
