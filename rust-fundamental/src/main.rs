455e789753dbc873df586ee2a0cedb5263111343
andiahmads <andi.fivesco@gmail.com>
2022-07-03 21:13:43 +0700

    complate assosiated function

--- rust-fundamental/src/main.rs
+++ rust-fundamental/src/main.rs
@@ -2,7 +2,7 @@
 // mod konsep_umum;
 mod ownership;
 mod reference_borowwing_slice;
-mod type_struct;
+// mod type_struct;
 mod method_assosiated;
 
 fn main() {
--- rust-fundamental/src/method_assosiated.rs
+++ rust-fundamental/src/method_assosiated.rs
@@ -12,12 +12,21 @@ mod method_assosiated {
         // tipe self merujuk kedalam struct Segitiga
         // maka setiap fungsi asosiated wajib menuliskan self
         // borrowed pada paramter self bertujuan untuk membaca data saja, dan tidak mengambil alih ownership
-        fn area(&self) -> u32 { // asosiated function
+
+        fn area(&self) -> u32 { // method (ditandai dengan paramter self)
             return self.height * self.width;
         }
 
         fn width(&self) -> bool {
             return self.width > 0;
+        }
+
+        fn can_hold(&self, other: &Segitiga) -> bool {
+            return self.width > other.width && self.height > other.height;
+        }
+
+        fn square(size:u32)-> Segitiga{ // assosiated function
+            return Segitiga{width: size, height: size};
         }
     }
 
@@ -38,13 +47,19 @@ mod method_assosiated {
             height: 14,
         };
 
-        println!("cant segitiga1 hold segitiga2");
+        println!("cant segitiga1 hold segitiga2? {}",segitiga1.can_hold(&segitiga2));
+        println!("cant segitiga1 hold segitiga3? {}",segitiga1.can_hold(&segitiga3));
 
-/*      let res = segitiga1.area();
+        //implementasi assosiated function
+        let sq = Segitiga::square(10); // ciri khas penggunaan assosiated function dg menggunakan ::
+        println!("hasil dari assosiated function {:?}",sq);
+
+
+      let res = segitiga1.area();
         println!("hasil =  {}", res);
         println!("width besar dari 0 =  {}", segitiga1.width());
         println!("hasil =  {:?}", segitiga1);
- */
+ 
 
         
 
