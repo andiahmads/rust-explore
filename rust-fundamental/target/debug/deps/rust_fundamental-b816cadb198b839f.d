e02b87099482bcc09d6756b5e5a969d434f31c9f
andiahmads <andi.fivesco@gmail.com>
2022-07-03 21:14:14 +0700

    initial commit

--- rust-fundamental/target/debug/deps/rust_fundamental-b816cadb198b839f.d
+++ rust-fundamental/target/debug/deps/rust_fundamental-b816cadb198b839f.d
@@ -1,9 +1,8 @@
-/home/domv/Code/rust/rust-explore/rust-fundamental/target/debug/deps/rust_fundamental-b816cadb198b839f.rmeta: src/main.rs src/ownership.rs src/reference_borowwing_slice.rs src/type_struct.rs src/method_assosiated.rs
+/home/domv/Code/rust/rust-explore/rust-fundamental/target/debug/deps/rust_fundamental-b816cadb198b839f.rmeta: src/main.rs src/ownership.rs src/reference_borowwing_slice.rs src/method_assosiated.rs
 
-/home/domv/Code/rust/rust-explore/rust-fundamental/target/debug/deps/rust_fundamental-b816cadb198b839f.d: src/main.rs src/ownership.rs src/reference_borowwing_slice.rs src/type_struct.rs src/method_assosiated.rs
+/home/domv/Code/rust/rust-explore/rust-fundamental/target/debug/deps/rust_fundamental-b816cadb198b839f.d: src/main.rs src/ownership.rs src/reference_borowwing_slice.rs src/method_assosiated.rs
 
 src/main.rs:
 src/ownership.rs:
 src/reference_borowwing_slice.rs:
-src/type_struct.rs:
 src/method_assosiated.rs:
