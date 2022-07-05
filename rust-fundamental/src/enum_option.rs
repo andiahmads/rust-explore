3d758b3682807a895c4884e87783c1478627e972
andiahmads <andi.fivesco@gmail.com>
2022-07-06 06:14:38 +0700

    complate learning enum

--- rust-fundamental/src/enum_option.rs
+++ rust-fundamental/src/enum_option.rs
@@ -4,14 +4,22 @@ mod enum_options {
 
     // contoh enum option
 
-    enum Option<T> { // Generic Type Parameter
-        Node,
-        Some(T), // akan menyimpan 1 data dari segala macam jenis type
-    }
+    // enum Option<T> { // Generic Type Parameter
+    //     Node,
+    //     Some(T), // akan menyimpan 1 data dari segala macam jenis type
+    // }
 
 
     #[test]
     fn test_enum_options() {
+        let some_number = Option::Some(5);
+        let some_str = Some("hello");
+        let number: Option<i32> = None; // untuk variant None wajib menyertakan Type Anotation
+
+        let x: i8 = 5;
+        let y: Option<i8> = Some(5);
+        let sum = x + y.unwrap();
+        println!("{:?}", sum);
 
     }
 
--- rust-fundamental/target/debug/deps/rust_fundamental-99da0dd7e8098619.d
+++ rust-fundamental/target/debug/deps/rust_fundamental-99da0dd7e8098619.d
@@ -1,9 +1,10 @@
-/home/domv/Code/rust/rust-explore/rust-fundamental/target/debug/deps/rust_fundamental-99da0dd7e8098619: src/main.rs src/ownership.rs src/reference_borowwing_slice.rs src/method_assosiated.rs src/enum_pattern.rs
+/home/domv/Code/rust/rust-explore/rust-fundamental/target/debug/deps/rust_fundamental-99da0dd7e8098619: src/main.rs src/ownership.rs src/reference_borowwing_slice.rs src/method_assosiated.rs src/enum_pattern.rs src/enum_option.rs
 
-/home/domv/Code/rust/rust-explore/rust-fundamental/target/debug/deps/rust_fundamental-99da0dd7e8098619.d: src/main.rs src/ownership.rs src/reference_borowwing_slice.rs src/method_assosiated.rs src/enum_pattern.rs
+/home/domv/Code/rust/rust-explore/rust-fundamental/target/debug/deps/rust_fundamental-99da0dd7e8098619.d: src/main.rs src/ownership.rs src/reference_borowwing_slice.rs src/method_assosiated.rs src/enum_pattern.rs src/enum_option.rs
 
 src/main.rs:
 src/ownership.rs:
 src/reference_borowwing_slice.rs:
 src/method_assosiated.rs:
 src/enum_pattern.rs:
+src/enum_option.rs:
